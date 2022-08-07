cfdf14a016c3d3c93896b38aba17db9849872165
jiyeong99 <1999wldud@gmail.com>
2022-08-07 14:45:53 +0900

    끝

--- 1회차/김지영/04.py
+++ 1회차/김지영/04.py
@@ -5,7 +5,29 @@ from pprint import pprint
 def recommendation(title):
     pass 
     # 여기에 코드를 작성합니다.  
-
+    BASE_URL = 'https://api.themoviedb.org/3'
+    path = '/search/movie'
+    params = {
+        'api_key': '66c53dabd7bc9afc53c2ca7eba855583',
+        'language' : 'ko-KR',
+        'query' : title
+    }
+    response = requests.get(BASE_URL+path, params=params).json()
+    try :
+        for tt in response['results']:
+            if title == tt['title']:
+                movie_id = tt['id']
+        
+        rcmd_path = f'/movie/{movie_id}/recommendations'
+        rcmd_response = requests.get(BASE_URL+rcmd_path, params=params).json()
+        rcmd_list = []
+        for name in rcmd_response['results']:
+            title = name['title']
+            rcmd_list.append(title)
+        return rcmd_list
+    except :
+        return None
+# exception handling...?
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
--- 1회차/김지영/test.py
+++ 1회차/김지영/test.py
@@ -1,20 +1,9 @@
-import requests
-from pprint import pprint
+numbers = list(map(int, input().split()))
+numbers.sort()
+total = 0
+count = 0
+for i in range(1, len(numbers)-1):
+    total += numbers[i]
+    count += 1
 
-
-def vote_average_movies():
-  pass 
-  # 여기에 코드를 작성합니다.  
-  BASE_URL = 'https://api.themoviedb.org/3'
-  path = '/movie/popular'
-  params = {
-      'api_key': '66c53dabd7bc9afc53c2ca7eba855583',
-      'language' : 'ko-KR'
-  }
-  response = requests.get(BASE_URL+path, params=params).json()
-  v = response.get('results') 
-  return v
-
-# 아래의 코드는 수정하지 않습니다.
-if __name__ == '__main__':
-    pprint(vote_average_movies())
+print(total//count)
